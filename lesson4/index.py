9b74fb9e79545fb6a96399bd7b8fe24ecdcfde47
roberthsu2003 <roberthsu2003@gmail.com>
2025-05-16 11:11:16 +0800

    modify

--- lesson4/index.py
+++ lesson4/index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello, Flask!</h1><p>這是我的第1頁</p>"
+    return render_template("index.html")
 
 @app.route("/user")
 def user():
